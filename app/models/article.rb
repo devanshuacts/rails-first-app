212bac8f7334cd8ab6071ae0e2dbbba64838b9de
Devanshu <devanshu.ds.singh@gmail.com>
2018-06-24 14:39:55 +0530

    migrate sqlite3 tables to mongodb documents type schema

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,11 @@
-class Article < ApplicationRecord
+class Article
+  include Mongoid::Document
+  include Mongoid::Timestamps
+
   has_many :comments, dependent: :destroy
   validates :title, presence: true, length: {minimum: 4}
+
+  field :title,   type: String
+  field :text,    type: String
 
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,10 @@
-class Comment < ApplicationRecord
+class Comment
+  include Mongoid::Document
+  include Mongoid::Timestamps
+
   belongs_to :article
+
+  field :commenter,   type: String
+  field :body,        type: String
+
 end
